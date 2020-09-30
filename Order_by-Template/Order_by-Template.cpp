14e8e600663edfa73c87a6029791e98c965f1f9c
dan <dan066@web.de>
2020-09-30 11:39:19 +0200

    Improve Order_by-Template

--- Order_by-Template/Order_by-Template.cpp
+++ Order_by-Template/Order_by-Template.cpp
@@ -3,15 +3,18 @@
 #include<algorithm>
 using namespace std;
 
-struct Person {
-	string name;
-	int age;
-	int height;
-};
+struct Person { string name; int age, height; };
 
-template<class F> //F == Person::*
-auto order_by(F f) { //&Person::alter
+template<class F> //F is Person::* e.g. &Person::age
+auto order_by_0(F f) {
 	return [f](const Person& smaller, const Person& bigger) {
+		return smaller.*f < bigger.*f;
+	};
+}
+
+template<class Class, class Type>
+auto order_by(Type Class::* f) {
+	return [f](const Class& smaller, const Class& bigger) {
 		return smaller.*f < bigger.*f;
 	};
 }
@@ -29,7 +32,8 @@ auto operator|(F1 f1, F2 f2) {
 }
 
 static void test() {
-
+	int Person::* test = &Person::age;
+	auto test2 = &Person::age;
 	vector<Person> persons = { {"a",50,170},{"b",65,170}, {"c",15,170}, {"d",65,190}, {"e",15,160}, {"f",65,180} };
 	auto result = max_element(persons.begin(), persons.end(), order_by(&Person::age));
 	result = max_element(persons.begin(), persons.end(), order_by(&Person::age) | order_by(&Person::height));
